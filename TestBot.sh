8ffaf5fb739924952e5450e1316c9982170b4b96
Dominik Baziuk <73165159+Foxtrot007-ai@users.noreply.github.com>
2023-06-16 22:16:57 +0200

    Update TestBot.sh

--- TestBot.sh
+++ TestBot.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -x
-myArray=("RandomPlayer" "OneTurnLookahead" "Minimax" "PrunedBFS")
+if [ $# -eq 0 ]
+  then
+    echo "./TestBot.sh <playername>"
+    exit 1
+fi
+myArray=("RandomPlayer" "OneTurnLookahead" "Minimax" "PrunedBFS" "WBukowski" "JMikolajczyk" "DBaziuk")
 echo $(date -u) >> TestOutput/output.txt
 for str in ${myArray[@]}; do
-    echo "    $str Bot vs MyPolicy Bot" >> TestOutput/output.txt
-    python3 example/Example_Match_All_bots.py $str | grep "MATCH RESULTS" | awk '{print "       " $0}' >> TestOutput/output.txt
+    echo "    $str Bot vs $1 Bot" >> TestOutput/output.txt
+    python3 example/Example_Match_All_bots.py $str $1 | grep "MATCH RESULTS" | awk '{print "       " $0}' >> TestOutput/output.txt
 done
