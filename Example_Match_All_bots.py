3890ea733ade2e60055a785dc1e30525529895f6
Dominik Baziuk <73165159+Foxtrot007-ai@users.noreply.github.com>
2023-06-16 22:16:22 +0200

    Update Example_Match_All_bots.py

--- Example_Match_All_bots.py
+++ Example_Match_All_bots.py
@@ -1,7 +1,9 @@
 import sys
 
 from agent.Example_Competitor import ExampleCompetitor
-from example.Example_MyPolicy import MyPolicy
+from example.Example_WBukowski import WBukowskiBattlePolicy
+from example.Example_JMikolajczyk import JMikolajczykBattlePolicy
+from example.Example_DBaziuk import DBaziukBattlePolicy
 from vgc.behaviour.BattlePolicies import Minimax
 from vgc.behaviour.BattlePolicies import RandomPlayer
 from vgc.behaviour.BattlePolicies import OneTurnLookahead
@@ -11,7 +13,7 @@ from vgc.competition.Competitor import CompetitorManager
 from vgc.util.generator.PkmRosterGenerators import RandomPkmRosterGenerator
 from vgc.util.generator.PkmTeamGenerators import RandomTeamFromRoster
 
-def match(botPolicy):
+def match(botPolicy, playerPolicy):
     roster = RandomPkmRosterGenerator().gen_roster()
     tg = RandomTeamFromRoster(roster)
     c0 = ExampleCompetitor("Enemy Bot")
@@ -19,7 +21,7 @@ def match(botPolicy):
     cm0 = CompetitorManager(c0)
     cm0.team = tg.get_team()
     c1 = ExampleCompetitor("My Bot")
-    c1._battle_policy = MyPolicy()  # switch agent to test
+    c1._battle_policy = playerPolicy  # switch agent to test
     cm1 = CompetitorManager(c1)
     cm1.team = tg.get_team()
     match = BattleMatch(cm0, cm1, debug=True)
@@ -27,19 +29,44 @@ def match(botPolicy):
 
 def main():
     # get argument from command
-    option = sys.argv[1]
-    print(option)
-    # decide which opponent to test self with
-    if option == "RandomPlayer":
-         match(RandomPlayer())
-    elif option == "OneTurnLookahead":
-         match(OneTurnLookahead())
-    elif option == "Minimax":
-         match(Minimax())
-    elif option == "PrunedBFS":
-        match(PrunedBFS())
-    else:
-        match(RandomPlayer())
+    bot_option = sys.argv[1]
+    player_option = sys.argv[2]
 
+    print(bot_option)
+    print(" vs ")
+    print(player_option)
+    
+    # decide which opponent to test self with
+    bot = None
+    if bot_option == "RandomPlayer":
+        bot = RandomPlayer()
+    elif bot_option == "OneTurnLookahead":
+        bot = OneTurnLookahead()
+    elif bot_option == "Minimax":
+        bot = Minimax()
+    elif bot_option == "PrunedBFS":
+        bot = PrunedBFS()
+    elif bot_option == "WBukowski":
+        bot = WBukowskiBattlePolicy()
+    elif bot_option == "JMikolajczyk":
+        bot = JMikolajczykBattlePolicy()
+    elif bot_option == "DBaziuk":
+        bot = DBaziukBattlePolicy()
+    else:
+        bot = RandomPlayer() #not implemented option
+
+    # decide which player 
+    player = None
+    if player_option == "WBukowski":
+        player = WBukowskiBattlePolicy()
+    elif player_option == "JMikolajczyk":
+        player = JMikolajczykBattlePolicy()
+    elif player_option == "DBaziuk":
+        player = DBaziukBattlePolicy()
+    else:
+        player = RandomPlayer() #not implemented option
+
+    #start game
+    match(bot, player)
 if __name__ == '__main__':
     main()
