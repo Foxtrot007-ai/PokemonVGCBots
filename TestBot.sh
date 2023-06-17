028a74340f2912e63087d48a314c914cf1d3211e
Dominik Baziuk <73165159+Foxtrot007-ai@users.noreply.github.com>
2023-06-17 17:13:45 +0200

    Update TestBot.sh

--- TestBot.sh
+++ TestBot.sh
@@ -2,12 +2,15 @@
 set -x
 if [ $# -eq 0 ]
   then
-    echo "./TestBot.sh <playername>"
+    echo "./TestBot.sh <playername> <numberOfMatches>"
     exit 1
 fi
+gamesToPlay=$2
 myArray=("RandomPlayer" "OneTurnLookahead" "Minimax" "PrunedBFS" "WBukowski" "JMikolajczyk" "DBaziuk")
 echo $(date -u) >> TestOutput/output.txt
 for str in ${myArray[@]}; do
     echo "    $str Bot vs $1 Bot" >> TestOutput/output.txt
-    python3 example/Example_Match_All_bots.py $str $1 | grep "MATCH RESULTS" | awk '{print "       " $0}' >> TestOutput/output.txt
+    python3 example/Example_Match_All_bots.py $str $1 $2 > temp.txt
+    cat temp.txt | grep "MATCH RESULTS"
+    cat temp.txt | grep "MATCH RESULTS" | sed -e 's/[^0-9]/ /g' -e 's/^ *//g' -e 's/ *$//g' | awk '{if ($1 < $2) SUM += 1; else SUM2 += 1} END { print "      MATCH RESULTS[" SUM2 ", " SUM "]"}' >> TestOutput/output.txt
 done
